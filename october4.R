383ec3a62e926efc95d9e8e772c2e841e402289d
SCOTT07186 <18757303528@163.com>
2023-10-22 23:11:59 -0400

    rr

--- october4.R
+++
@@ -1,4 +0,0 @@
-strawberry <- read.csv(file = "strawberry_oct4.csv", header = T)
-strwb_census<- strawberry %>% filter(strawberry[,2],Program=="CENSUS")
-strwb_survey<- strawberry |>filter(Program=="SURVEY")
-cva <- strwb_census$value
